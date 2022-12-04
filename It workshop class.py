828b52eb86d49613dacb024cd5aaed66c754b4f3
Dilpreet <92905896+dfordp@users.noreply.github.com>
2022-12-04 14:33:59 +0530

    Update It workshop class.py

--- It workshop class.py
+++ It workshop class.py
@@ -113,3 +113,44 @@ def integrate (lower_range, upper_range, n):
     return ((grid_space / 2) * s)
 
 print(integrate(5, 10, 10000))   
+
+
+#WAP # Python program for implementation
+# of Bisection Method for
+# solving equations
+# The function is 2x3+4x2+x-3
+def func(x):
+    return 2*x**3+4*x**2+x-3
+
+# Prints root of func(x)
+# with error of EPSILON
+def bisection(a,b):
+
+    if (func(a) * func(b) >= 0):
+        print("You have not assumed right a and b\n")
+        return
+
+    c = a
+    while ((b-a) >= 0.01):
+
+        # Find middle point
+        c = (a+b)/2
+
+        # Check if middle point is root
+        if (func(c) == 0.0):
+            break
+
+        # Decide the side to repeat the steps
+        if (func(c)*func(a) < 0):
+            b = c
+        else:
+            a = c
+
+    print("The value of root is : ","%.4f"%c)
+
+# Driver code
+# Initial values assumed
+a =-200
+b = 300
+bisection(a, b)
+
